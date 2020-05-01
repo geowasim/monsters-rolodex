d392ef83c9cb4b5c016fa05c46883b2e3ab0d02b
Wasim <waseem_wmgh@yahoo.com>
2020-05-01 14:31:32 +0200

    Updates

---
+++ precache-manifest.de766cb9d23e0c1ffea7f9097834db03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1fac4eb8daab1c48b7a6b7ce40b065a",
+    "url": "/geowasim/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "52f3c3dcef820bd28db9",
+    "url": "/geowasim/monsters-rolodex/static/css/main.078caf4d.chunk.css"
+  },
+  {
+    "revision": "9f834d1fc4df12fb31cd",
+    "url": "/geowasim/monsters-rolodex/static/js/2.308474c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/geowasim/monsters-rolodex/static/js/2.308474c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52f3c3dcef820bd28db9",
+    "url": "/geowasim/monsters-rolodex/static/js/main.10978ea1.chunk.js"
+  },
+  {
+    "revision": "f01325c43703f8e3dc64",
+    "url": "/geowasim/monsters-rolodex/static/js/runtime-main.0eff33f7.js"
+  }
+]);
